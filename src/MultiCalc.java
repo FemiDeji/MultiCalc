0b26a0eeba7aaac7d53570e7f52d1976dd2859a4
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:07:01 -0800

    Initial commit

---
+++ src/MultiCalc.java
@@ -0,0 +1,11 @@
+public class MultiCalc{
+    public static void main(String[] args){
+        int a, b, c, d;
+        a=2;
+        b=4;
+        c=6;
+        d=8;
+        int multiplication = a*b*c*d;
+        System.out.println(multiplication);
+    }
+}
